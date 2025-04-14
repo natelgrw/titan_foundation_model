3671f22e14392c41b0470e6c23f010299ebbe161
natelgrw <nate.lgrw@gmail.com>
2025-04-14 07:12:36 -0400

    editing

--- turbo_optimizer/script_multi1.sh
+++ turbo_optimizer/script_multi1.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-export PYTHONPATH="${PYTHONPATH}:/homes/dkochar/migration_project/open-source/nathan/new_folder_7nmsingle_ended_cascode_current_mirror/working_current"
-export BASE_TMP_DIR=/homes/dkochar/migration_project/open-source/nathan/new_folder_7nmsingle_ended_cascode_current_mirror/base_tmp/
-
+export PYTHONPATH="${PYTHONPATH}:/homes/natelgrw/Documents/turbo_circuit_optimizer/turbo_optimizer/working_current"
+export BASE_TMP_DIR=/homes/natelgrw/Documents/turbo_circuit_optimizer/turbo_optimizer/base_tmp
 	#cat /homes/dkochar/migration_project/open-source/new_folder_7nmsingle_ended_cascode_current_mirror/working_current/eval_engines/spectre/specs_test/single_ended_cascode_current_mirror_initial.yaml > /homes/dkochar/migration_project/open-source/new_folder_7nmsingle_ended_cascode_current_mirror/working_current/eval_engines/spectre/specs_test/single_ended_cascode_current_mirror.yaml
 	
-	source /homes/dkochar/.bashrc #conda for script setup
+	source /homes/natelgrw/.bashrc #conda for script setup
 	conda activate rlenv38 #conda environment activate
-	python /homes/dkochar/migration_project/open-source/nathan/new_folder_7nmsingle_ended_cascode_current_mirror/working_current/sample/random_sample_turbo.py > /homes/dkochar/migration_project/open-source/nathan/new_folder_7nmsingle_ended_cascode_current_mirror/dummy.txt #run the actual turbo code
+	python /homes/natelgrw/Documents/turbo_circuit_optimizer/turbo_optimizer/working_current/sample/random_sample_turbo.py #run the actual turbo code
 	conda deactivate
 
 
